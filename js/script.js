cd139e4107e6019bd6e8e47a577f9ea801064a0a
marisa_b <marisakbrantley@gmail.com>
2022-04-20 20:47:34 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Targeting my GitHub profile information
+const overview = document.querySelector('.overview')
+
+const username = 'marisabrantley';
+
+// Fetches GitHub profile data
+const gitUserData = async () => {
+    const userInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    // console.log(data);
+    displayData(data);
+};
+
+gitUserData();
+
+// To display fetched GitHub profile data
+const displayData = (data) => {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('user-info');
+    newDiv.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+        `;
+    overview.append(newDiv);
+};
+
